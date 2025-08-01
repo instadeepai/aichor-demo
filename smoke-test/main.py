99eedb1fcf1ef1814317ecb5308af4c0579ae12e
Adrien Pichard <a.pichard@instadeep.com>
2025-08-01 11:14:43 +0200

    chore(smoke-test): remove mentions of tf and batchjobs operators

--- smoke-test/main.py
+++ smoke-test/main.py
@@ -3,26 +3,22 @@ import time
 
 from src.operators.jax import jaxop
 from src.operators.ray import rayop
-from src.operators.tf import tfop
 from src.operators.pytorch import pytorchop
 from src.operators.xgboost import xgboostop
-from src.operators.batchjob import batchjobop
 from src.operators.jobset import jobsetop
 
 OPERATOR_TABLE = {
     "ray": rayop,
     "kuberay": rayop,
-    "tf": tfop,
     "jax": jaxop,
     "pytorch": pytorchop,
     "xgboost": xgboostop,
-    "batchjob": batchjobop,
     "jobset": jobsetop
 }
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='AIchor Smoke test on any operator')
-    parser.add_argument("--operator", type=str, default="tf", choices=OPERATOR_TABLE.keys(),help="operator name")
+    parser.add_argument("--operator", type=str, default="jobset", choices=OPERATOR_TABLE.keys(),help="operator name")
     parser.add_argument("--sleep", type=int, default="0", help="sleep time in seconds")
     parser.add_argument("--tb-write", type=bool, default=False, help="test write to tensorboard")
 
