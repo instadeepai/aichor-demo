ad171a47eee6dcc86f6aa280ec25bb086b3eaae7
Mohammad Mamdouhi <m.mamdouhi@instadeep.com>
2025-09-05 11:32:06 +0200

    chore(jobset): fix the ) so it doesn't trip up the f-string

--- smoke-test/src/operators/jobset.py
+++ smoke-test/src/operators/jobset.py
@@ -6,5 +6,6 @@ def jobsetop(tb_write):
 
 def print_jobset_env(tb_write):
     print("job completion index: ", os.environ.get("JOB_GLOBAL_INDEX"))
+    idx = os.getenv('JOB_GLOBAL_INDEX')
     if tb_write:
-        dummy_tb_write(f"From job_index {os.environ.get("JOB_GLOBAL_INDEX")}")
+        dummy_tb_write(f"From job_index {idx}")
