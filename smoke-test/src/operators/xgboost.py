99dd7c4de2421497956f8ad65a7c996f10cc9eb1
Mohammad Mamdouhi <m.mamdouhi@instadeep.com>
2025-09-24 17:22:34 +0200

    feat(xgboost/pytorch): update the manifests and docs in accordance to latest AIchor changes

--- smoke-test/src/operators/xgboost.py
+++ smoke-test/src/operators/xgboost.py
@@ -8,8 +8,6 @@ def xgboostop(tb_write: bool):
     print("MASTER_ADDR: ", os.environ.get("MASTER_ADDR"))
     print("WORLD_SIZE: ", os.environ.get("WORLD_SIZE"))
     print("RANK: ", rank)
-    print("WORKER_PORT: ", os.environ.get("WORKER_PORT"))
-    print("WORKER_ADDRS: ", os.environ.get("WORKER_ADDRS"))
 
     if tb_write:
         dummy_tb_write(f"from rank {rank}")
